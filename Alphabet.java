f9e6fae08526babaf0ee85ac34480657d59841c0
SivaAdityaNarra <87695903+SivaAdityaNarra@users.noreply.github.com>
2021-09-04 13:49:38 +0530

    Add files via upload

---
+++ Alphabet.java
@@ -0,0 +1,36 @@
+class Aplhabet
+{
+	public static void main(String args[])
+	{ char a='a';
+	  char b='B';
+	int valueA=(int)a;
+	int valueB=(int)b;
+	if (valueA<95)
+	{a=(char)(valueA+32);
+	}
+	else{
+	
+	}
+	if (valueB<95){
+	b=(char)(valueB+32);}
+	else{
+		}
+
+	if(a>b)
+	{System.out.println(b);
+	 System.out.println(a);
+	
+	}
+	else
+	{ System.out.println(a);
+	 System.out.println(b);
+
+
+	}
+
+
+
+	}
+
+
+}
